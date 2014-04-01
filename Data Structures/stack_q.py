b7b28f5c690a5e33324a84dc5451560e8e1df4c1
Yan Cao <cyandterry@hotmail.com>
2014-03-31 21:48:35 -0400

    Done with two stack to a queue

--- Data Structures/stack_q.py
+++ Data Structures/stack_q.py
@@ -4,3 +4,28 @@
 # pop() as pop
 # For Queue, use from collections import deque as queue
 # Use append() as push, use popleft() as pop
+
+class my_queue():
+    def __init__(self):
+        self.stack_a = []
+        self.stack_b = []
+        self.storing_a = True
+
+    def push(self, data):
+        if storing_a:
+            self.stack_a.append(data)
+        else:
+            self.stack_b.append(data)
+
+    def pop(self):
+        # Data is in A, so first push data to B and pop the last one in queue
+        if storing_a:
+            while len(a)>0:
+                b.append(a.pop())
+            storing_a = False
+            return b.pop()
+        else:
+            while len(b)>0:
+                a.append(b.pop())
+            storing_a = True
+            return a.pop()
