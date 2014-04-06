56538543411ffb18bef147457042bb29861fb2ad
Yan Cao <cyandterry@hotmail.com>
2014-04-05 21:47:48 -0400

    Almost completed bt

--- Data Structures/bt.py
+++ Data Structures/bt.py
@@ -79,6 +79,49 @@ def is_balance(root):
         return False
     return is_balance(root.left) and is_balance(root.right)
 
+def covers(root, p):
+    if root is None:
+        return False
+    if root == p:
+        return True
+    return covers(root.left, p) or covers(root.right, p)
+
+# store the result of cover so that we don't need to calculate it again
+def first_common_ancestor(root, p, q):
+    # p q not a child of root
+    if not (covers(root, p) and covers(root,q)):
+        return None
+    p_is_left = covers(root.left, p)
+    q_is_left = covers(root.left, p)
+    # Diff sides
+    if p_is_left != q_is_left:
+        return root
+    elif p_is_left:
+        return first_common_ancestor(root.left, p, q)
+    else:
+        return first_common_ancestor(root.right, p, q)
+        
+def is_subtree(t1, t2):
+    if t1 = None:
+        return False
+    
+    if t1.data == t2.data:
+        if is_match(t1, t2):
+            return True
+
+    return is_subtree(t1.left, t2) or is_subtree(t1.right, t2)
+
+def is_match(t1,t2):
+    if t1 is None and t2 is None:
+        return True
+    elif (t1 is not None and t2 is None) or (t1 is None and t2 is not None):
+        return False
+    else:
+        if t1.data != t2.data:
+            return False
+        else:
+            return is_match(t1.left, t2.left) and is_match(t1.right, t2.right)
+
 if __name__ == '__main__':
     data_list = [1,3,4,5,6,8,9,10,13,14,17,18]
     root = create_minimum_BST(data_list, 0, len(data_list)-1)
