1007911be665ba7065254e4b8215e1b0b6887a0f
Yan Cao <cyandterry@hotmail.com>
2014-04-13 22:41:41 -0400

    Added insertion sort, need to complete merge sort

--- search_n_sort.py
+++ search_n_sort.py
@@ -25,10 +25,43 @@ def selection_sort(data_list):
                 print data_list
 
 def insertion_sort(data_list):
-    pass
+    for i in range( len(data_list) ):
+        temp = data_list[i]
+        k = i
+        while data_list[k-1] > temp and k > 0:
+            data_list[k] = data_list[k-1]
+            k -= 1
+        data_list[k] = temp
+        print data_list
 
-def merge_sort(data_list):
-    pass
+def merge_sort(data_list, low, high):
+    if low < high:
+        middle = (low + high) / 2
+        merge_sort(data_list, low, middle)
+        merge_sort(data_list, middle+1, high)
+        merge(data_list, low, middle, high)
+
+def merge(data_list, low, middle, high):
+    helper = [None] * len(data_list)
+    for i in range(low, high+1):
+        helper[i] = data_list[i]
+
+    helper_left = low
+    helper_right = middle + 1
+    current = low
+
+    while helper_left <= middle and helper_right <= high:
+        if helper[helper_left] < helper[helper_right]:
+            data_list[current] = helper[helper_left]
+            helper_left += 1
+        else:
+            data_list[current] = helper[helper_right]
+        current += 1
+
+    remaining = middle - helper_left
+    for i in range(remaining+1):
+        data_list[current+i] = helper[helper_left+i]
+
 
 # These are adata_listdata_list for quick sort
 def quick_sort(data_list):
@@ -53,5 +86,7 @@ if __name__ == '__main__':
                 break
     print data_list
     #selection_sort(data_list)
-    bubble_sort(data_list)
+    #bubble_sort(data_list)
+    #insertion_sort(data_list)
+    merge_sort(data_list, 0, len(data_list)-1)
     print data_list
