0deadec4fa3bc2576301b9675f12da562425178e
Yan Cao <cyandterry@hotmail.com>
2014-06-07 17:05:43 -0400

    Done with 43

--- leetcode.py
+++ leetcode.py
@@ -848,8 +848,34 @@ def search_2d_matrix(matrix, target):
     return False
 
 # 43. Set Matrix Zeroes
-def set_matirx_0():
-    pass
+# There's a better way which can save the space of doing so'
+def set_matirx_0(matrix):
+    m = len(matrix[0])
+    n = len(matrix)
+    zero_row = False
+    zero_col = False
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                if i == 0:
+                    zero_row = True
+                if j == 0:
+                    zero_col = True
+                matrix[i][0] = matrix[0][j] = 0
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+    if zero_row:
+        for j in range(m):
+            matrix[0][j] = 0
+    if zero_colum:
+        for i in range(n):
+            matrix[i][0] = 0
+
+    return matrix
 
 # 44. Path Sum
 def path_sum():
