29d9550a68462ab07521335e1c463152884f5350
Yan Cao <cyandterry@hotmail.com>
2014-06-28 17:01:13 -0400

    Fixed small thing

--- Leetcode/reverse_words_in_a_string.py
+++ Leetcode/reverse_words_in_a_string.py
@@ -1,15 +1,24 @@
 """
-Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+Given an input string, reverse the string word by word.
 
-For example, given
-s = 'leetcode',
-dict = ['leet', 'code'].
+For example,
+Given s = 'the sky is blue',
+return 'blue is sky the'.
 
-Return true because 'leetcode' can be segmented as 'leet code'.
+Clarification:
+What constitutes a word?
+A sequence of non-space characters constitutes a word.
+Could the input string contain leading or trailing spaces?
+Yes. However, your reversed string should not contain leading or trailing spaces.
+How about multiple spaces between two words?
+Reduce them to a single space in the reversed string.
+
 """
 
 class Solution:
     # @param s, a string
     # @return a string
+
+    # This is a python easy way
     def reverseWords(self, s):
         return ' '.join(s.split()[::-1])
