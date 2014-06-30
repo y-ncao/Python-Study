7e9e9c4a6dd1f400a1cc56232e7f986a3c8422c7
Yan Cao <cyandterry@hotmail.com>
2014-06-29 22:33:53 -0400

    Added hashtable

--- Concept_Implement/HashTable.py
+++ Concept_Implement/HashTable.py
@@ -1,5 +1,3 @@
-
-
 class Item:
     key = None
     value = None
@@ -26,9 +24,10 @@ class HashTable:
 
     def insert(self, item):
         hashKey = self.hashing(item.key)
-        self.hash_table[hashKey].append(item)
-        self.count += 1
-
+        self.hash_table[hashKey].append(item) # This is using closed addressing, each item hash to a list
+        self.count += 1                       # then traverse tehe list
+                                              # Alternative is to increase size of hashtable, or use open
+                                              # addressing, server ways
     def get(self, key):
         hashKey = self.hashing(key)
         for item in self.hash_table[hashKey]:
@@ -42,5 +41,15 @@ class HashTable:
         for i, item in enumerate(self.hash_talbe[hashKey]):
             if item.key == key:
                 del hash_table[hashKey][i]
+                self.count -= 1
                 return
         print "Not in Table"
+
+    def print(self):
+        for i in range(tablesize):
+            print 'In table ', i
+            for j in range(len(self.hash_table[i])):
+                print self.hash_table[i][j]
+
+    def getEntryCount():
+        return self.count
