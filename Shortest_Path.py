4d8c36ee2967f233e64263420199175416e7a812
Yan Cao <cyandterry@hotmail.com>
2014-07-02 23:05:20 -0400

    Added my bad interview question

---
+++ Shortest_Path.py
@@ -0,0 +1,17 @@
+M= len(map)
+N = len(map[0])
+visited = {}
+# Maybe still not the best result
+def findPath(map, current_point, end_point, length, visited):
+    if current_point == end_point:
+        return length
+    if current_point in visited:
+        return INT_MAX
+    if map[current_point] == 'x':
+        return INT_MAX
+    visited[current_point] = True
+    x, y = current_point
+    return min( findPath(map, (x+1,y  ), end_point, length+ 1, visited),
+                findPath(map, (x-1,y  ), end_point, length+ 1, visited),
+                findPath(map, (x  ,y-1), end_point, length+ 1, visited),
+                findPath(map, (x  ,y+1), end_point, length+ 1, visited) )
