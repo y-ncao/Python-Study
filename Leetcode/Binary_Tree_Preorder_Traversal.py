2c14520ed78207a2bcd2b2f16a0015aab43866aa
Yan Cao <cyandterry@hotmail.com>
2014-07-07 22:12:37 -0400

    Completed Preorder

--- Leetcode/Binary_Tree_Preorder_Traversal.py
+++ Leetcode/Binary_Tree_Preorder_Traversal.py
@@ -1,5 +1,5 @@
 """
-Given a binary tree, return the inorder traversal of its nodes' values.
+Given a binary tree, return the preorder traversal of its nodes' values.
 
 For example:
 Given binary tree {1,#,2,3},
@@ -8,7 +8,7 @@ Given binary tree {1,#,2,3},
      2
     /
    3
-return [1,3,2].
+return [1,2,3].
 
 Note: Recursive solution is trivial, could you do it iteratively?
 """
@@ -23,31 +23,31 @@ Note: Recursive solution is trivial, could you do it iteratively?
 class Solution:
     # @param root, a tree node
     # @return a list of integers
-    def inorderTraversal(self, root):
-        return self.inorderTraversal_1(root)
+    def preorderTraversal(self, root):
+        return self.preorderTraversal_1(root)
 
-    def inorderTraversal_1(self, root):
+    def preorderTraversal_1(self, root):
         stack = []
         current = root
         res = []
-        while current is not None or len(stack) > 0:
+        while current is not None or len(stack)>0:
             if current is not None:
+                res.append(current.val)
                 stack.append(current)
                 current = current.left
-            elif len(stack) > 0:
+            elif len(stack)>0:
                 current = stack.pop()
-                res.append(current.val)
                 current = current.right
         return res
 
-    def inorderTraversal_2(self, root):
+    def preorderTraversal_2(self, root):
         res = []
-        self.inorderTraversal_rec(root, res)
+        self.preorderTraversal_rec(root, res)
         return res
 
-    def inorderTraversal_rec(self, root, res):
+    def preorderTraversal_rec(self, root, res):
         if root is None:
             return
-        self.inorderTraversal_rec(root.left, res)
         res.append(root.val)
-        self.inorderTraversal_rec(root.right, res)
+        self.preorderTraversal_rec(root.left, res)
+        self.preorderTraversal_rec(root.right, res)
