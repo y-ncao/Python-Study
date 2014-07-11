8d332e11e29450c2badf1e8d9f60ecadd1a7f265
Yan Cao <cyandterry@hotmail.com>
2014-07-11 00:08:19 -0400

    Done with isSymmetric

--- Leetcode/Symmetric_Tree.py
+++ Leetcode/Symmetric_Tree.py
@@ -29,6 +29,9 @@ class Solution:
     # @param root, a tree node
     # @return a boolean
     def isSymmetric(self, root):
+        return self.isSymmetric_2(root)
+
+    def isSymmetric_1(self, root):
         if root is None:
             return True
         return self.symmetric_helper(root.left, root.right)
@@ -36,10 +39,27 @@ class Solution:
     def symmetric_helper(self, n1, n2):
         if n1 is None and n2 is None:
             return True
-        if n1 is None or n2 is None:
-            return False
-        if n1.val != n2.val:
+        if n1 is None or n2 is None or n1.val != n2.val:
             return False
         return self.symmetric_helper(n1.left, n2.right) and self.symmetric_helper(n1.right, n2.left)
 
-    # This is accepted. Next is do it iteratively
+    # No need to use two queues here, just one but pop twice would be fine
+    # Keep in mind which node should be pop first
+    def isSymmetric_2(self, root):
+        if root is None:
+            return True
+        queue = collections.deque()
+        queue.append(root.left)
+        queue.append(root.right)
+        while len(queue)>0:
+            t1 = queue.popleft()
+            t2 = queue.popleft()
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None or t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        return True
