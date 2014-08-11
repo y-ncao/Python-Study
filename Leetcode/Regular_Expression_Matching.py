970041e52b783f0e175a2633a15965baa8b2a0f5
Yan Cao <cyandterry@hotmail.com>
2014-08-11 16:00:58 -0400

    need to read those two ways

---
+++ Leetcode/Regular_Expression_Matching.py
@@ -0,0 +1,72 @@
+"""
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+"""
+
+class Solution:
+    # @return a boolean
+    def isMatch(self, s, p):
+        return self.isMatch_helper(s, p, 0, 0)
+
+    def isMatch_helper(self, s, p, i, j):
+        if j >= len(p):
+            return s[i] >= len(p)
+
+        if s[i] == p[j] or ( p[j] == '.' and i < len(s)):
+            if j+1 < len(p) and p[j+1] != '*':
+                return self.isMatch_helper(s, p, i+1, j+1)
+            else:
+                return self.isMatch_helper(s, p, i+1, j) or self.isMatch_helper(s, p, i, j+2)
+
+        return j+1 < len(p) and p[j+1] == '*' and self.isMatch(s, p, i, j+2)
+
+    # Recursion
+    def isMatch(self, s, p):
+        if len(p)==0: return len(s)==0
+        if len(p)==1 or p[1]!='*':
+            if len(s)==0 or (s[0]!=p[0] and p[0]!='.'):
+                return False
+            return self.isMatch(s[1:],p[1:])
+        else:
+            i=-1; length=len(s)
+            while i<length and (i<0 or p[0]=='.' or p[0]==s[i]):
+                if self.isMatch(s[i+1:],p[2:]): return True
+                i+=1
+            return False
+
+
+
+    #dp
+    def isMatch(self, s, p):
+        dp=[[False for i in range(len(p)+1)] for j in range(len(s)+1)]
+        dp[0][0]=True
+        for i in range(1,len(p)+1):
+            if p[i-1]=='*':
+                if i>=2:
+                    dp[0][i]=dp[0][i-2]
+        for i in range(1,len(s)+1):
+            for j in range(1,len(p)+1):
+                if p[j-1]=='.':
+                    dp[i][j]=dp[i-1][j-1]
+                elif p[j-1]=='*':
+                    dp[i][j]=dp[i][j-1] or dp[i][j-2] or (dp[i-1][j] and (s[i-1]==p[j-2] or p[j-2]=='.'))
+                else:
+                    dp[i][j]=dp[i-1][j-1] and s[i-1]==p[j-1]
+        return dp[len(s)][len(p)]
+复制代码
