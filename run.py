2a4050843580f605b66b73ae1397cadb94efa062
Yan Cao <cyandterry@hotmail.com>
2014-08-13 01:38:15 -0400

    Completed this

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-file_name = 'Decode_Ways'
-func_name = 'numDecodings'
+file_name = 'Maximal_Rectangle'
+func_name = 'maximalRectangle'
 
 import importlib
 module = importlib.import_module('Leetcode.%s' % file_name)
 instance = module.Solution()
 
-print getattr(instance, func_name)('12')
+print getattr(instance, func_name)(['00010010','00010010','00010010','00000000','00010010'])
