4085ce55d7f418aad76a1af142444c8f0218b817
Yan Cao <cyandterry@hotmail.com>
2014-08-13 23:45:51 -0400

    Added the evaludate...

---
+++ Leetcode/Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,34 @@
+"""
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+"""
+
+class Solution:
+    # @param tokens, a list of string
+    # @return an integer
+    def evalRPN(self, tokens):
+        stack = []
+        operators = '+-*/'
+        for i, token in enumerate(tokens):
+            if token in operators:
+                num_2 = stack.pop()
+                num_1 = stack.pop()
+                stack.append(self.calculate(num_1, num_2, token))
+            else:
+                stack.append(int(token))
+        return stack[0]
+
+    def calculate(self, num_1, num_2, token):
+        if token == '+':
+            return num_1 + num_2
+        if token == '-':
+            return num_1 - num_2
+        if token == '*':
+            return num_1 * num_2
+        if token == '/':
+            return int(num_1 * 1.0 / num_2) # This is the trick part, need to notice
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-file_name = 'Valid_Number'
-func_name = 'isNumber'
+file_name = 'Evaluate_Reverse_Polish_Notation'
+func_name = 'evalRPN'
 
 import importlib
 module = importlib.import_module('Leetcode.%s' % file_name)
 instance = module.Solution()
 
-print getattr(instance, func_name)("-1.")
+print getattr(instance, func_name)(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
