8e9b0b67627ecbf490da161f21ac7cd70039ee72
Yan Cao <cyandterry@hotmail.com>
2014-08-14 01:08:15 -0400

    Really couldn't understand them

--- Leetcode/Longest_Palindromic_Substring.py
+++ Leetcode/Longest_Palindromic_Substring.py
@@ -5,3 +5,56 @@ Given a string S, find the longest palindromic substring in S. You may assume th
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
+        N = len(s)
+        dp = [ [ False for j in range(N)] for i in range(N)]
+        max_length = 0
+        ret = s[0]
+        # dp[i][j] means s[i:j] is palindrome
+        for j in range(N):
+            for i in range(j):
+                dp[i][j] == (s[i] == s[j] and ( j - i < 2 or dp[i+1][j-1]))
+                if dp[i][j] and max_length < j-i+1:
+                    if j - i + 1 > max_length:
+                        max_length = j - i + 1
+                        ret = s[i:j]
+            dp[j][j] = True
+        return ret
+
+"""
+    def longestPalindrome(self, s):
+        longest, mid = "", (len(s) - 1) / 2
+        i, j = mid, mid
+        while i >= 0 and j < len(s):
+            args = [(s, i, i), (s, i, i + 1), (s, j, j), (s, j, j + 1)]
+            for arg in args:
+                tmp = self.longestPalindromeByAxis(*arg)
+                if len(tmp) > len(longest):
+                    longest = tmp
+            if len(longest) >= i * 2:
+                return longest
+            i, j = i - 1, j + 1
+        return longest
+
+    def longestPalindromeByAxis(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left, right = left - 1, right + 1
+        return s[left + 1 : right]
+
+    def longestPalindrome(self, s):
+        arr = ['$', '#']
+        for i in range(len(s)):
+            arr.append(s[i])
+            arr.append('#')
+        p = [0] * len(arr)
+        mx, pos, ansp = 0, 0, 0
+        for i in range(1, len(arr)):
+            p[i] = min(mx - i, p[2 * pos - i]) if mx > i else 1
+            while p[i] + i < len(arr) and arr[i + p[i]] == arr[i - p[i]]:
+                p[i] += 1
+            if p[i] + i > mx:
+                mx, pos = p[i] + i, i
+            if p[i] > p[ansp]:
+                ansp = i
+        st = (ansp - p[ansp] + 1) // 2
+        return s[st:st + p[ansp] - 1]
+"""
--- Leetcode/Substring_with_Concatenation_of_All_Words.py
+++ Leetcode/Substring_with_Concatenation_of_All_Words.py
@@ -14,6 +14,47 @@ class Solution:
     # @param L, a list of string
     # @return a list of integer
     def findSubstring(self, S, L):
+        LS, LL, LL0 = len(S), len(L), len(L[0])
+        did, ids, dl = {}, 0, {}
+        for s in L:
+            id = did.get(s, -1)
+            if id == -1:
+                 ids = ids + 1
+                 id = ids
+                 did[s] = id
+            dl[id] = dl.get(id, 0) + 1
+
+        pos, ans = [0] * LS, []
+        for k, v in did.items():
+            f = S.find(k)
+            while f != -1:
+                pos[f] = v
+                f = S.find(k, f + 1)
+
+        for sp in range(LL0):
+            np, pp, tot, dt = sp, sp, 0, {}
+            while np < LS:
+                t = pos[np]
+                if t == 0:
+                    tot, dt = 0, {}
+                    pp, np = np + LL0, np + LL0
+                elif dt.get(t, 0) < dl[t]:
+                    dt[t] = dt.get(t, 0) + 1
+                    tot = tot + 1
+                    if tot == LL:
+                        ans.append(pp)
+                    np = np + LL0
+                else:
+                    while pos[pp] != t:
+                        tot = tot - 1
+                        dt[pos[pp]] -= 1
+                        pp = pp + LL0
+                    pp = pp + LL0
+                    dt[t] -= 1
+                    tot = tot - 1
+        return ans
+
+"""
         if len(S) == 0 or len(L) == 0:
             return []
         length = len(L[0])
@@ -27,3 +68,4 @@ class Solution:
                     dp[i] += dp[i-length]
                 ret = max(ret, dp[i])
         return ret
+"""
