32ea72a052e6fcddc6b3469411c75559748c2c10
Yan Cao <cyandterry@hotmail.com>
2014-08-14 20:03:54 -0400

    Added LRU_Cache using OrderedDict

--- Leetcode/LRU_Cache.py
+++ Leetcode/LRU_Cache.py
@@ -10,6 +10,30 @@ class LRUCache:
     # @param capacity, an integer
     def __init__(self, capacity):
         self.capacity = capacity
+        self.cache = collections.OrderedDict()
+
+    # @return an integer
+    def get(self, key):
+        if key in self.cache:
+            value = self.cache[key]
+            del self.cache[key]
+            self.cache[key] = value
+            return value
+        return -1
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, value):
+        if key not in self.cache and len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+        elif key in self.cache:
+            del self.cache[key]
+        self.cache[key] = value
+"""
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.cache = {}
         self.queue = []
 
@@ -17,7 +41,8 @@ class LRUCache:
     def get(self, key):
         if key in self.cache:
             self.queue.remove(key)
-            return self.cache.pop(key)
+            self.queue.append(key)
+            return self.cache[key]
         return -1
 
     # @param key, an integer
@@ -27,11 +52,14 @@ class LRUCache:
         if key in self.cache:
             self.queue.remove(key)
         elif len(self.cache) >= self.capacity:
-            self.cache.pop(queue.pop(0))
-            self.cache[key] = value
+            remove = self.queue.pop(0)
+            self.cache.pop(remove)
 
         self.queue.append(key)
         self.cache[key] = value
-
+"""
     # Checked online, so changed to use python's ordered dictionary
     # So obsoleted queue
+    # Also, there was some understanding mistake
+    # When get a key, it won't delete the key, but just reorder it to highier rank
+    # Just like a cache. Don't think it as a dict
