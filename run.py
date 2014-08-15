08c9e0cae28148e7af1edffa91d758048b70af1f
Yan Cao <cyandterry@hotmail.com>
2014-08-14 23:09:56 -0400

    Added Interleaving string

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-file_name = 'Text_Justification'
-func_name = 'fullJustify'
+file_name = 'Interleaving_String'
+func_name = 'isInterleave'
 
 import importlib
 module = importlib.import_module('Leetcode.%s' % file_name)
 instance = module.Solution()
 
-print getattr(instance, func_name)(["a","b","c","d","e"], 3)
+print getattr(instance, func_name)("aabcc","dbbca","aadbbbaccc")
