96438b4284bc34b903e8b8080c3e19b758214733
Yan Cao <cyandterry@hotmail.com>
2014-08-16 17:31:29 -0400

    Adding SR right now

---
+++ Leetcode/Surrounded_Regions.py
@@ -0,0 +1,66 @@
+"""
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+For example,
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+"""
+
+class Solution:
+    # @param board, a 2D array
+    # Capture all regions by modifying the input board in-place.
+    # Do not return any value.
+    def solve(self, board):
+        if len(board) == 0 or len(board[0]) == 0: # This is sooooo keng
+            return board
+        M = len(board)
+        N = len(board[0])
+        for i in range(M):
+            for j in range(N):
+                if i == 0 or i == M-1 or j == 0 or j == N-1:
+                    self.bfs(board, i, j)
+        for i in range(M):
+            for j in range(N):
+                if board[i][j] == 'V':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+    def bfs(self, board, row, col):
+        if (board[row][col] != 'O'):
+            return
+        q = []
+        q.append((row, col))
+        while len(q) > 0:
+            i, j = q.pop(0)
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                continue
+            if board[i][j] != 'O':
+                continue
+            board[i][j] = 'V'
+            q.push((i-1, j))
+            q.push((i+1, j))
+            q.push((i, j-1))
+            q.push((i, j+1))
+
+    # DFS will cause stack overflow
+    def dfs(self, board, row, col):
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
+            return
+        if board[row][col] != 'O':
+            return
+        board[row][col] = 'V'
+        self.dfs(board, row+1, col)
+        self.dfs(board, row-1, col)
+        self.dfs(board, row, col+1)
+        self.dfs(board, row, col-1)
