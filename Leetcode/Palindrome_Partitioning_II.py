b66fd9044ddbc0fea6f5be742793bdee9abe5018
Yan Cao <cyandterry@hotmail.com>
2014-08-17 00:35:00 -0400

    Added palindrom II

---
+++ Leetcode/Palindrome_Partitioning_II.py
@@ -0,0 +1,25 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+"""
+
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def minCut(self, s):
+        dp = [0 for i in range(len(s)+1)]
+        p = [[False for i in range(len(s))] for j in range(len(s))]
+        for i in range(len(s)+1):
+            dp[i] = len(s) - i
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (((j - i) < 2) or p[i+1][j-1]):
+                    p[i][j] = True
+                    dp[i] = min(1+dp[j+1], dp[i])
+        return dp[0]-1
+    # This solution will get a TLE
+    # Need to find a better way
