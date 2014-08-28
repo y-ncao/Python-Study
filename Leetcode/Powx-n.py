bf481a19bac1bfa7f236351c3d288fe99f4df763
Yan Cao <cyandterry@hotmail.com>
2014-08-28 17:05:50 -0400

    added space to operator

--- Leetcode/Powx-n.py
+++ Leetcode/Powx-n.py
@@ -9,9 +9,9 @@ class Solution:
     def pow(self, x, n):
         if x == 0 or x == 1:
             return x
-        elif x < 0 and n%2 == 0:
+        elif x < 0 and n % 2 == 0:
             return self.pow(-x, n)
-        elif x < 0 and n%2 ==1:
+        elif x < 0 and n % 2 ==1:
             return self.pow(-x, n) * (-1)
         elif n < 0:
             return 1.0 / self.pow(x, -n)
@@ -25,3 +25,4 @@ class Solution:
             return half * half
         else:
             return half * half * x
+    # Note to use the half var to make the code clean
