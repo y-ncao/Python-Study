981e79c53e7bbbc6ff3ee7f8a1370afa1117cb28
Yan Cao <cyandterry@hotmail.com>
2014-09-07 19:34:30 -0400

    Added Print Numbers with Five

--- Interviews/Print_Numbers_With_Five.py
+++ Interviews/Print_Numbers_With_Five.py
@@ -1,6 +1,24 @@
 # 9/7/2014 From mitbbs for Groupon http://www.mitbbs.com/article_t/JobHunting/32651839.html
 
-'''
+"""
 写一个function，对于参数n，输出从0到n之间所有含5的数字。
 func(30) 应该输出5，15，25
-'''
+"""
+
+def find_five(n):
+    ret = []
+    helper(ret, [], n)
+    return ret
+
+def helper(ret, res, n):
+    if len(res) > 0 and ( int(''.join(res)) > n or len(res) > len(str(n)) ):
+        return
+    if '5' in res and int(''.join(res)) not in ret:
+        ret.append(int(''.join(res)))
+
+    for i in range(10):
+        res.append(str(i))
+        helper(ret, res, n)
+        res.pop()
+
+print find_five(60)
