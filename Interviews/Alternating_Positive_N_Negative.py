9f98ac599ce97655f6fe4049a4275e5c8ce19de2
Yan Cao <cyandterry@hotmail.com>
2014-09-09 00:53:26 -0400

    Added Alterating question

---
+++ Interviews/Alternating_Positive_N_Negative.py
@@ -0,0 +1,17 @@
+"""
+Given an array of positive and negative numbers, arrange them in an alternate fashion such that every positive number is followed by negative and vice-versa maintaining the order of appearance.
+Number of positive and negative numbers need not be equal. If there are more positive numbers they appear at the end of the array. If there are more negative numbers, they too appear in the end of the array.
+
+Example:
+
+Input:  arr[] = {1, 2, 3, -4, -1, 4}
+Output: arr[] = {-4, 1, -1, 2, 3, 4}
+
+Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0}
+
+Found online, also NC has marked this to high frequency
+
+[Solution](http://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/)
+"""
+
