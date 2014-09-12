aebeb8ad7d7b2a02372bdcb25e335a6695db90e0
Yan Cao <cyandterry@hotmail.com>
2014-09-12 02:05:28 -0400

    Removed previous versions

--- Leetcode/Minimum_Path_Sum.py
+++ Leetcode/Minimum_Path_Sum.py
@@ -8,8 +8,28 @@ class Solution:
     # @param grid, a list of lists of integers
     # @return an integer
     def minPathSum(self, grid):
-        return self.minPathSum_2(grid)
+        M = len(grid)
+        N = len(grid[0])
+        dp = [ [0 for j in range(N)] for i in range(M)]
+        for i in range(M):
+            for j in range(N):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        return dp[M-1][N-1]
 
+    # Note:
+    # 1. dp[i][j] means from (0, 0) to (i, j) the min path sum
+    # 2. init: dp[i][0] = dp[i-1][0]+grid[i][j], dp[0][j] += dp[0][j-1]+grid[i][j]
+    # 3. func: dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+    # 4. ret: dp[m-1][n-1]
+"""
+All Previous work. No need to worry
     def minPathSum_1(self, grid):
         M = len(grid)
         N = len(grid[0])
@@ -32,13 +52,13 @@ class Solution:
     # M = len(grid)
     # N = len(grid[0])
 
-    """
+
     Given the dynamic programming formula f[i][j]=min(f[i-1][j],f[i][j-1])+grid[i][j]:
 
     Assume that you are populating the table row by row, the current value (f[i][j]) will be used immediately in the calculation of f[i][j+1], so there is no need to store all the previous column values.
 
     Therefore, you can do it in linear space complexity.
-    """
+
     def minPathSum_2(self, grid):
         M = len(grid)
         N = len(grid[0])
@@ -55,3 +75,4 @@ class Solution:
 
     # This is a bit tricky. Read the above how to simplify this
     # The key is we are doing this for j ... so we can just j-1
+"""
