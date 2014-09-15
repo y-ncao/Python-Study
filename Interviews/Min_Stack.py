0f5147c2085f63815fd35e90368496f491c79994
Yan Cao <cyandterry@hotmail.com>
2014-09-15 01:58:15 -0400

    Added Stacks problems

--- Interviews/Min_Stack.py
+++ Interviews/Min_Stack.py
@@ -1,4 +1,34 @@
 """
-see http://www.geeksforgeeks.org/design-and-implement-special-stack-data-structure/
-
+#####From NC Class 7 Data Structures, slides 8
+[Solution](http://www.geeksforgeeks.org/design-and-implement-special-stack-data-structure/)
 """
+
+class MinStack():
+    def __init__(self, size):
+        self.data_stack = []
+        self.min_stack = []
+        self.size = size
+
+    def is_full(self):
+        return len(self.data_stack) == self.size
+
+    def is_empty(self):
+        return not self.data_stack
+
+    def push(self, data):
+        if self.is_full():
+            return False
+        self.data_stack.append(data)
+        if not self.min_stack or data <= self.min_stack[-1]:
+            self.min_stack.append(data)
+
+    def pop(self):
+        if self.is_empty():
+            return False
+        data = self.data_stack.pop()
+        if data == self.min_stack[-1]:
+            self.min_stack.pop()
+        return data
+
+    def get_min(self):
+        return self.min_stack[-1]
--- Interviews/Queue_by_Two_Stacks.py
+++ Interviews/Queue_by_Two_Stacks.py
@@ -1,3 +1,23 @@
 """
 Implement a Queue by using two stacks. Support O(1) push, pop, top
 """
+
+class Queue():
+    def __init__(self):
+        self.inbox = []
+        self.outbox = []
+
+    def push(self, num):
+        self.inbox.append(num)
+
+    def pop(self):
+        if not self.outbox:
+            while len(self.inbox) > 0:
+                self.outbox.append(self.inbox.pop())
+        return self.outbox.pop()
+
+    def top(self):
+        if not self.outbox:
+            while len(self.inbox) > 0:
+                self.outbox.append(self.inbox.pop())
+        return self.outbox[-1]
