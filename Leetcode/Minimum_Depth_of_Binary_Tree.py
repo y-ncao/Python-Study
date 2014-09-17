d2b767fc332745e119fea082c4beaebe8bb7c1d1
Yan Cao <cyandterry@hotmail.com>
2014-09-17 00:29:39 -0400

    Refactored the min path

--- Leetcode/Minimum_Depth_of_Binary_Tree.py
+++ Leetcode/Minimum_Depth_of_Binary_Tree.py
@@ -15,35 +15,13 @@ class Solution:
     # @param root, a tree node
     # @return an integer
     def minDepth(self, root):
+        if not root:
+            return 0
         return self.minDepth_rec(root)
 
     def minDepth_rec(self, root):
-        if root is None:
-            return 0
+        if not root:
+            return 9223372036854775807
         if root.left is None and root.right is None:
             return 1
-        if root.left is None:
-            return self.minDepth_rec(root.right) + 1
-        if root.right is None:
-            return self.minDepth_rec(root.left) + 1
         return min(self.minDepth_rec(root.left), self.minDepth_rec(root.right)) + 1
-
-    # Must do it in this way.
-    # It's different from the maxDepth because in max, we search for max
-    # In this min, the min depth is root to leaf node
-    # A leaf node is node does not have any child.
-    # Those has one child cannot be called leaf node
-
-    def minDepth_iter(self, root):
-        if root is None:
-            return 0
-        queue = []
-        queue.append( (root, 1) )
-        while len(queue) > 0:
-            node, depth = queue.pop(0)
-            if node.left is None and node.right is None:
-                return depth
-            if node.left is not None:
-                queue.append( (node.left, depth+1) )
-            if node.right is not None:
-                queue.append( (node.right, depth+1) )
