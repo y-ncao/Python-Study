e9f4a63df325dd52aa81f4818efe613d6ae2a8ce
Yan Cao <cyandterry@hotmail.com>
2014-09-24 00:26:33 -0400

    Added 2D matrix plus

--- Leetcode/Search_a_2D_Matrix.py
+++ Leetcode/Search_a_2D_Matrix.py
@@ -35,3 +35,54 @@ class Solution:
             else:
                 end = mid - 1
         return False
+
+```
+#####Search a 2D Matrix II
+What if we allow duplicate in the matrix and need to find out all the posisitons
+```
+
+def search_matrix_II(matrix, target):
+    lb = search_for_bound(matrix, target, True)
+    if lb == -1:
+        return False
+    rb = search_for_bound(matrix, target, False)
+    res = []
+    for i in range(lb, rb+1):
+        res.append((i/N, i%N))
+    return res
+
+def search_for_bound(matrix, target, is_lower_bound):
+    M = len(matrix)
+    N = len(matrix[0])
+    start = 0
+    end = M*N - 1
+    while start + 1 < end:
+        mid = (start+end) / 2
+        if matrix[mid/N][mid%N] == target:
+            if is_lower_bound:
+                end = mid
+            else:
+                start = mid
+        elif matrix[mid/N][mid%N] < target:
+            start = mid
+        else:
+            end = mid
+
+    if is_lower_bound:
+        if matrix[start/N][start%N] == target:
+            return start
+        elif matrix[end/N][end%N] == target:
+            return end
+    else:
+        if matrix[end/N][end%N] == target:
+            return end
+        elif matrix[start/N][start%N] == target:
+            return start
+
+    return -1
+
+```
+Note:
+1. from [mitbbs](http://www.mitbbs.com/article_t/JobHunting/32775405.html) that answer check mid-1 == mid
+2. I don't think this is a good way. So need to discuss here.
+```
