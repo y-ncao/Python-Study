1a2a11ad34f7320c11a57740ead97f764f56d271
Yan Cao <cyandterry@hotmail.com>
2020-01-26 23:43:02 -0800

    Adding 890

---
+++ Ninja/Leetcode/890_Find_and_Replace_Pattern.py
@@ -0,0 +1,45 @@
+"""
+You have a list of words and a pattern, and you want to know which words in words matches the pattern.
+
+A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
+
+(Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to the same letter.)
+
+Return a list of the words in words that match the given pattern. 
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+Output: ["mee","aqq"]
+Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}.
+"ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation,
+since a and b map to the same letter.
+ 
+
+Note:
+
+1 <= words.length <= 50
+1 <= pattern.length = words[i].length <= 20
+"""
+
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        return [word for word in words if self.isMatch(word, pattern)]
+
+    def isMatch(self, word, pattern):
+        if len(word) != len(pattern) or len(set(word)) != len(set(pattern)):
+            return False
+
+        d = {}
+        for w, p in zip(word, pattern):
+            if p not in d:
+                d[p] = w
+            else:
+                if d[p] != w:
+                    return False
+
+        return True
