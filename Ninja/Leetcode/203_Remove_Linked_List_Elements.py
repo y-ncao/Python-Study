7efa21d0d491a478fed993f56f75389aa73353a6
Yan Cao <cyandterry@hotmail.com>
2020-02-23 22:18:39 -0800

    Adding 203

---
+++ Ninja/Leetcode/203_Remove_Linked_List_Elements.py
@@ -0,0 +1,28 @@
+"""
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        while head:
+            if head.val == val:
+                prev.next = head.next
+            else:
+                prev = head
+            head = head.next
+
+        return dummy.next
