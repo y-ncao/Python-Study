7acb47397294c9df8cada710034a1670b1828f8b
Yan Cao <cyandterry@hotmail.com>
2020-02-23 16:50:19 -0800

    update 141

--- Ninja/Leetcode/141_Linked_List_Cycle.py
+++ Ninja/Leetcode/141_Linked_List_Cycle.py
@@ -1,8 +1,24 @@
 """
 Given a linked list, determine if it has a cycle in it.
 
-Follow up:
-Can you solve it without using extra space?
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
 """
 
 # Definition for singly-linked list.
@@ -12,14 +28,17 @@ Can you solve it without using extra space?
 #         self.next = None
 
 class Solution:
-    # @param head, a ListNode
-    # @return a boolean
-    def hasCycle(self, head):
-        slow = head
-        fast = head
-        while fast is not None and fast.next is not None:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = fast = head
+        while(
+            fast and
+            fast.next
+        ):
             slow = slow.next
             fast = fast.next.next
+
             if fast == slow:
                 return True
         return False
+
+    # Note that the fast == slow should be checked on the later.
