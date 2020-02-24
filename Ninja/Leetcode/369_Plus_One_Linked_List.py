a447db24fe201cf72e57ef0414cdc71eb8e46233
Yan Cao <cyandterry@hotmail.com>
2020-02-23 22:50:13 -0800

    Adding 369

---
+++ Ninja/Leetcode/369_Plus_One_Linked_List.py
@@ -0,0 +1,21 @@
+"""
+Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example :
+
+Input: [1,2,3]
+Output: [1,2,4]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
