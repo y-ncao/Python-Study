65aec3f450b359013adcca46a4b6c5c482ac5372
Yan Cao <cyandterry@hotmail.com>
2020-02-24 21:33:06 -0800

    Adding 200

--- Ninja/Leetcode/200_Number_of_Islands.py
+++ Ninja/Leetcode/200_Number_of_Islands.py
@@ -23,17 +23,31 @@ Output: 3
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[False] * len(grid[0])] * len(grid)
+        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
         islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1 and not visited[i][j]:
+                if grid[i][j] == "1" and not visited[i][j]:
                     islands += 1
-                    self.DFS((i, j), grid)
+                    self.DFS((i, j), grid, visited)
 
         return islands
 
-    def DFS(self, current, grid):
+    def DFS(self, current, grid, visited):
         directions = [(0,1), (1,0), (0, -1), (-1, 0)]
 
+        x, y = current
+        if visited[x][y]:
+            return
 
+        visited[x][y] = True
+        for d in directions:
+            next_x = x + d[0]
+            next_y = y + d[1]
+
+            if (
+                0 <= next_x < len(grid) and
+                0 <= next_y < len(grid[0]) and
+                grid[next_x][next_y] == "1"
+            ):
+                self.DFS((next_x, next_y), grid, visited)
